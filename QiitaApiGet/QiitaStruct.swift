dca77b825f45ab54ec569fca7274ce6002c4fb38
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-11 21:05:30 +0900

    QiitaでTitleとUserを取得する

---
+++ QiitaApiGet/QiitaStruct.swift
@@ -0,0 +1,14 @@
+//
+//  QiitaStruct.swift
+//  QiitaApiGet
+//
+//  Created by 近藤宏輝 on 2020/02/11.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+struct QiitsStruct: Codable {
+    var title: String
+    var user: User
+}
---
+++ QiitaApiGet/QiitaUser.swift
@@ -0,0 +1,13 @@
+//
+//  QiitaUser.swift
+//  QiitaApiGet
+//
+//  Created by 近藤宏輝 on 2020/02/11.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: String
+}
